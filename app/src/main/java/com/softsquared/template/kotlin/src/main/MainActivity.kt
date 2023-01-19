808505572cefd56c14150044f556b55e106f5c6c
SoobinJ <soobin08011812@gmail.com>
2023-01-19 23:39:59 +0900

    feat: 상단바, 스위치버튼

--- app/src/main/java/com/softsquared/template/kotlin/src/main/MainActivity.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/MainActivity.kt
@@ -1,43 +1,43 @@
 package com.softsquared.template.kotlin.src.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayout
 import com.softsquared.template.kotlin.R
 import com.softsquared.template.kotlin.databinding.ActivityMainHomeBinding
-import com.softsquared.template.kotlin.src.main.mainHome.ViewPagerAdapter
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainHomeBinding
 
-    private val tabTitleArray = arrayOf(
-        "#전체 ",
-        "#밥 ",
-        "#여행 ",
-        "#스터디 ",
-        "#취미 ",
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var viewPager = binding.viewPager
-        var tabLayout = binding.tabLayout
         var toolbar = binding.toolbar
 
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        viewPager.adapter=ViewPagerAdapter(supportFragmentManager,lifecycle)
+        binding.mainHomeTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                var text = tab!!.text.toString()
+                Log.d("test1",tab!!.text.toString())
+            }
 
-        TabLayoutMediator(tabLayout,viewPager) { tab,position ->
-            tab.text = tabTitleArray[position]
-        }.attach()
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
