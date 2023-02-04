4e01ac4a92a11661d2011da6da8c62ae8aa90a4b
Woony <91375979+Chanwoo-Jeong@users.noreply.github.com>
2023-02-04 16:32:32 +0900

    Revert "Feature/woony"

--- app/src/main/java/com/softsquared/template/kotlin/src/main/Chatting/ChatData.kt
+++
@@ -1,8 +0,0 @@
-package com.softsquared.template.kotlin.src.main.Chatting
-
-data class ChatData(
-    val mynickName : String = "" ,
-    val msg : String = "" ,
-    val time : String =""
-)
-
--- app/src/main/java/com/softsquared/template/kotlin/src/main/Note/NoteData.kt
+++
@@ -1,7 +0,0 @@
-package com.softsquared.template.kotlin.src.main.Note
-
-data class NoteData(
-    val from : String = "" ,
-    val to : String = "" ,
-)
-
