d424be065b4e581a65e6638ffdf3f972ffe96ef2
전수빈 <soobin08011812@gmail.com>
2023-02-06 23:14:11 +0900

    feat: 네이버지도 현재위치 진행중

--- app/src/main/java/com/softsquared/template/kotlin/src/main/mainHome/mapFragment.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/mainHome/mapFragment.kt
@@ -1,41 +1,51 @@
 package com.softsquared.template.kotlin.src.main.mainHome
 
-import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RelativeLayout
-import com.naver.maps.map.NaverMap
 import androidx.fragment.app.Fragment
-import com.naver.maps.map.MapView
+import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
+import java.util.*
 
-
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
 /**
  * A simple [Fragment] subclass.
  * Use the [mapFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class mapFragment : Fragment() {
+class mapFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var mapView: MapView
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var naverMap:NaverMap
+    private lateinit var locationSource: FusedLocationSource
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(com.softsquared.template.kotlin.R.id.map_view) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(com.softsquared.template.kotlin.R.id.map_view, it).commit()
+            }
+        mapFragment.getMapAsync(this)
 
+        locationSource= FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onCreateView(
@@ -47,24 +57,15 @@ class mapFragment : Fragment() {
         return rootView
     }
 
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+//        naverMap.locationTrackingMode=LocationTrackingMode.Follow
+    }
+
     companion object {
         lateinit var naverMap: NaverMap
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment mapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            mapFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
+
 }
