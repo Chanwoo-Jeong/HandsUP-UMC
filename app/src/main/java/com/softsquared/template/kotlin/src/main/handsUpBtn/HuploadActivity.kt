de1c76ada14e71cf0508ffef5b7bf4593c3cd06c
Chanwoo-Jeong <lordroad38@naver.com>
2023-02-09 20:19:02 +0900

    Feat : 핸즈업버튼 tag & location 값 추출하기

--- app/src/main/java/com/softsquared/template/kotlin/src/main/handsUpBtn/HuploadActivity.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/handsUpBtn/HuploadActivity.kt
@@ -5,14 +5,19 @@ import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.internal.InternalTokenProvider
 import com.softsquared.template.kotlin.databinding.HandsupbtnBinding
 
 
 
-class HuploadActivity : AppCompatActivity() {
+class HuploadActivity : AppCompatActivity()  {
     private lateinit var viewbinding: HandsupbtnBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,13 +43,30 @@ class HuploadActivity : AppCompatActivity() {
                 // [SeekBar 터치 종료]
             }
         })
-        viewbinding.toggleButton.setOnClickListener{
-            if(viewbinding.toggleButton.isChecked){
-                viewbinding.toggleButton.setTextColor(Color.parseColor("#215449"))
-            } else{
-                viewbinding.toggleButton.setTextColor(Color.parseColor("#000000"))
+
+
+        val btn1 = viewbinding.toggleButton
+        val btn2 = viewbinding.toggleButton2
+        val btn3 = viewbinding.toggleButton3
+        val btn4 = viewbinding.toggleButton4
+        val btn5 = viewbinding.toggleButton5
+
+        var btnarr = arrayOf(btn1, btn2, btn3, btn4, btn5)
+
+
+        for( btn in btnarr){
+            btn.setOnClickListener{
+                for(btn in btnarr) {
+                    btn.setTextColor(Color.parseColor("#000000"))
+                }
+                if (btn.isChecked) {
+                    btn.setTextColor(Color.parseColor("#d9534f"))
+                } else {
+                    btn.setTextColor(Color.parseColor("#000000"))
+                }
             }
         }
+
         viewbinding.uploadBtn.setOnClickListener {
 
             var location = if(viewbinding.locationSwitch.isChecked){
@@ -53,14 +75,37 @@ class HuploadActivity : AppCompatActivity() {
                 "위치비밀"
             }
 
+            var tag = btnarr.filter { btn -> btn.isChecked === true }
+            var duration = viewbinding.seekBar.getProgress()
+            Log.d("duration", duration.toString())
+
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra("name",viewbinding.nickname.getText().toString())
             intent.putExtra("location" , location)
             intent.putExtra("postContent",viewbinding.Content.getText().toString())
+            intent.putExtra("tag",tag[0].toString())
+            intent.putExtra("duration",duration)
             setResult(RESULT_OK,intent)
             finish()
             //액티비티를 끝내는 코드
         }
 
     }
+
+//
+//    private fun setTextColorChange(btn: ToggleButton) {
+//        if (btn != null) {
+//            if (btn.isChecked) {
+//                btn.setTextColor(Color.parseColor("#215449"))
+//                return btn.getText().toString()
+//            } else {
+//                btn.setTextColor(Color.parseColor("#000000"))
+//                return ""
+//            }
+//
+//        }
+//        return ""
+//    }
+
+
 }
